666164aef8f71d71da41410e60f3861e6cd8ce2f
Luiz Leão <luizleao@gmail.com>
2019-09-20 11:23:53 -0300

    Paginacao
    Alteração no componente paginacao

--- templates/bootstrap3/dir/componentes/comp.Paginacao.php
+++ templates/bootstrap3/dir/componentes/comp.Paginacao.php
@@ -1,6 +1,7 @@
 <nav aria-label="Page navigation">
 	<ul class="pagination">
-		<li class="<?=($_REQUEST['pag'] == 1) ? "disabled" : ""?>"><a href="?pag=<?=$_REQUEST['pag']-1?>">&lt; Anterior</a></li>
+		<li class="<?=($_REQUEST['pag'] == 1) ? "disabled" : ""?>"><a href="?" aria-label="Next"><span aria-hidden="true">&laquo;</span></a>
+		<li class="<?=($_REQUEST['pag'] == 1) ? "disabled" : ""?>"><a href="?pag=<?=$_REQUEST['pag']-1?>">&lsaquo;</a></li>
 <?php 
 for($i=1; $i<=$numPags; $i++){
 ?>
@@ -8,6 +9,8 @@ for($i=1; $i<=$numPags; $i++){
 <?php 
 }
 ?>
-		<li class="next <?=($_REQUEST['pag'] == $numPags) ? "disabled" : ""?>"><a href="?pag=<?=$_REQUEST['pag']+1?>" rel="next">Próximo &gt;</a></li>
+		<li class="next <?=($_REQUEST['pag'] == $numPags) ? "disabled" : ""?>"><a href="?pag=<?=$_REQUEST['pag']+1?>" rel="next">&rsaquo;</a></li>
+		<li class="next <?=($_REQUEST['pag'] == $numPags) ? "disabled" : ""?>"><a href="?pag=<?=$numPags?>" aria-label="Next"><span aria-hidden="true">&raquo;</span></a>
+    </li>
 	</ul><!-- /.pagination -->
 </nav>
