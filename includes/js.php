f23d82b26e383443734bf9fc5c3f2f7dea427186
Luiz Leão <luizleao@gmail.com>
2019-09-20 16:06:18 -0300

    Update js.php
    Ajustes

--- includes/js.php
+++ includes/js.php
@@ -1,6 +1,6 @@
 <!--JavaScript at end of body for optimized loading-->
-<script src="js/highcharts.js"></script>
 <script src="js/jquery-2.1.1.min.js"></script>
+<script src="js/highcharts.js"></script>
 <script src="js/materialize.min.js"></script>
 <script src="js/init.js"></script>
 <script src="js/comum.js"></script>
